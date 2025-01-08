8433e9d4937cb8bc3c066a88d8558e8e93ce67d2
Vyshnavi-dot <balusuvyshu@gmail.com>
2025-01-08 19:14:07 +0530

    Nested_list

---
+++ Day6_problems.java
@@ -0,0 +1,15 @@
+public class Day6_problems {
+    public static void main(String[]args){
+        for (int i = 0; i<3; i++) {
+            for(int j=0; j<4; j++){
+                System.out.print("a");
+            }
+            System.out.println();
+        }
+        
+    }
+}
+
+
+
+
